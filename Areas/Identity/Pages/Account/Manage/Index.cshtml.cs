b2c30d90b03712ef52a5dc18d4b2aac069b4933b
Tom Nguyen <tndallas9517@gmail.com>
2022-12-14 17:45:37 -0600

    221214 improved Home Index view

--- Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -151,6 +151,8 @@ namespace BugTrackerMVC.Areas.Identity.Pages.Account.Manage
                 }
             }
 
+            await _userManager.UpdateAsync(user);
+
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
             return RedirectToPage();
